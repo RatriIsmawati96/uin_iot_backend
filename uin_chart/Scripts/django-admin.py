0596108b326539109d2973cfc66f96930f917226
RatriIsmawati96 <56153676+ratriismawati96@users.noreply.github.com>
2019-10-25 17:11:11 +0700

    iot_backend

---
+++ uin_chart/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\hp\PycharmProjects\uin_iot_backend\uin_chart\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
